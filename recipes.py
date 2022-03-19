3d76236c12647243bb192b9178cc56977a920d92
Piotr Cenda <piotr.cenda222@gmail.com>
2022-03-19 13:20:30 +0100

    async fetching

--- recipes.py
+++ recipes.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
-import requests
+import asyncio
+import aiohttp
+import time
 
 # TODO: zrobić do wysyłania zapytań i parsowania funkcję asynchroniczną!!!
 # TODO: zastanowić się, czy starcza nam takie proste przejście po każdym indeksie, czy coś lepszego co uruchamia selenium
@@ -15,6 +17,8 @@ bo dane są ukryte za html, a js je odkrywa, a nie są dodatkowo ściągane z se
 po przemyśleniu też raczej nie trzeba zaczynać z scrapy, bo to jakieś za potężne narzędzie
 '''
 
+start_time = time.perf_counter()
+
 # urls range
 base_url = r"https://cosylab.iiitd.edu.in/recipedb/search_recipeInfo/"
 start_id = 10000
@@ -23,45 +27,52 @@ end_id = 99989
 # to prettify strings
 delchars = "!@#$%^&*()_+-=~`{}|[]:\"\\;'<>?/\t\r\n"
 
-response = requests.get(base_url + str(start_id))
-# print(response.text) # jprd ale syf mają xd
 
-# parsing html
-soup = BeautifulSoup(response.text, 'lxml')
-recipe_title = soup.find('h3', attrs={"style": "font-family: Helvetica;"})
-print(recipe_title.text)
-
-recipe_ingridients_table = soup.find('table', attrs={"id": "myTable"}).find_all('tr')
-
-for ingridient_info in recipe_ingridients_table[1:]:
-    infos = ingridient_info.find_all('td')[0:3]
-    ingridient = infos[0].text
-    quantity = infos[1].text
-    unit = infos[2].text
-    
-    print(f"{quantity} {unit} of {ingridient}")
-    
-recipe_instructions = soup.find('div', attrs={"id": "steps"})
-print(recipe_instructions.text.translate(str.maketrans('', '', delchars)).strip()) # brzydkie dziwne znaki i odstępy, najlepiej dodać jeden regex xd
+async def fetch(session, url):
+    response = await session.get(url)
+    response.raise_for_status()
+    html = await response.text()
+    return html
 
 
-# asynchronous solution
-# loop = asyncio.get_event_loop()
+async def parse(session, url):
+    try:
+        html = await fetch(session=session, url=url)
+    except Exception as e:
+        print(e) # trzzeba pewnie rozwinąć
+    else:
+        soup = BeautifulSoup(html, 'lxml')
+        recipe_title = soup.find('h3', attrs={"style": "font-family: Helvetica;"})
+        print(recipe_title.text)
 
-# def do_thing(params):
-#     async def get_rpc_info_and_do_chores(id):
-#         # do things
-#         response = perform_grpc_call(id)
-#         do_chores(response)
+        recipe_ingridients_table = soup.find('table', attrs={"id": "myTable"}).find_all('tr')
 
-#     async def get_httpapi_info_and_do_chores(id):
-#         # do things
-#         response = requests.get(URL)
-#         do_chores(response)
+        for ingridient_info in recipe_ingridients_table[1:]:
+            infos = ingridient_info.find_all('td')[0:3]
+            ingridient = infos[0].text
+            quantity = infos[1].text
+            unit = infos[2].text
+            
+            print(f"{quantity} {unit} of {ingridient}")
+            
+        recipe_instructions = soup.find('div', attrs={"id": "steps"})
+        print(recipe_instructions.text.translate(str.maketrans('', '', delchars)).strip()) # brzydkie dziwne znaki i odstępy, może regex? xd
 
-#     async_tasks = []
-#     for element in list(params.list_of_things):
-#        async_tasks.append(loop.create_task(get_chan_info_and_do_chores(id)))
-#        async_tasks.append(loop.create_task(get_httpapi_info_and_do_chores(ch_id)))
+        return [recipe_title, recipe_ingridients_table, recipe_instructions]
 
-#     loop.run_until_complete(asyncio.gather(*async_tasks))
+
+async def async_main():
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        
+        for i in range(start_id, start_id+10):
+            url = base_url + str(i)
+            tasks.append(asyncio.ensure_future(parse(session, url)))
+
+        await asyncio.gather(*tasks)
+        
+
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(async_main())
+    print(f"\ntime elapsed:  {time.perf_counter() - start_time}")
